dee04086d24632e1cb6356649d16be35d85daf71
carled7 <carlos_ed03@outlook.com>
2021-04-04 15:23:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 let outputNumber = [];
-let basesConverter = [2, 8];
-
+let basesConverter = [2, 8, 16];
 let digits;
+let inputBase;
+
 function receiveNumber() {
+    //receive the number and its base selected by the user
     let inputNumber = document.getElementById("inputNumber");
     inputNumber = inputNumber.value.toString();
     digits = inputNumber.split('');
-    let base = document.getElementById("base");
-    if (base.value != 10) {
-        inputNumber = toDecimal(digits, base.value);
-        console.log(inputNumber);
-    }
-    else {
+    inputBase = document.getElementById("base");
 
+    //if the base is different to 10, it will be converted to it
+    if (inputBase.value != 10) {
+        inputNumber = toDecimal(digits, inputBase.value);
     }
-    //fromDecimal(inputNumber);
     printNumber(inputNumber);
 }
 
 function printNumber(num) {
+    //this function converts the 'inputNumber' into the default bases
     for (base of basesConverter) {
-        
-        console.log(fromDecimal(num, base));
+        if (base != inputBase.value) {
+            console.log(fromDecimal(num, base));
+        }
+
     }
 }
 
 function toDecimal(digits, base) {
+
+    //this function converts the 'inputNumber' into the 10 base
     digits.reverse();
     let i = 0;
     let numDecimal = 0;
     if (base > 10) {
+        //if the base is greater than 10, it is necessary to add new algorisms (a-f)
         digits.forEach(replaceChar)
     }
     for (digit of digits) {
@@ -39,7 +44,14 @@ function toDecimal(digits, base) {
     return numDecimal;
 }
 function fromDecimal(num, base) {
+
+    //this function converts numbers in the 10 base to others
     outputNumber = [];
+
+    if (num < base) {
+        outputNumber.push(num);
+        outputNumber.forEach(replaceNum);
+    }
     while (num >= base) {
         outputNumber.push(num % base);
         num = Math.floor(num / base);
@@ -47,9 +59,14 @@ function fromDecimal(num, base) {
             outputNumber.push(num);
         }
     }
+    if (base > 10) {
+        outputNumber.forEach(replaceNum);
+    }
     return outputNumber;
 }
 function replaceChar(digit) {
+
+    //this function replace the letters (A-F) to the correspondent number
     let modChar = digit.toUpperCase();
     switch (modChar) {
         case "A":
@@ -69,6 +86,32 @@ function replaceChar(digit) {
             break;
         case 'F':
             digits.splice(digits.indexOf(digit), 1, 15);
+            break;
+    }
+}
+
+function replaceNum(digit) {
+
+    //this function replace the digits greater than 10 to letters
+    let modNum = Number(digit);
+    switch (modNum) {
+        case 10:
+            outputNumber.splice(outputNumber.indexOf(digit), 1, 'A');
+            break;
+        case 11:
+            outputNumber.splice(outputNumber.indexOf(digit), 1, 'B');
+            break;
+        case 12:
+            outputNumber.splice(outputNumber.indexOf(digit), 1, 'C');
+            break;
+        case 13:
+            outputNumber.splice(outputNumber.indexOf(digit), 1, 'D');
+            break;
+        case 14:
+            outputNumber.splice(outputNumber.indexOf(digit), 1, 'E');
+            break;
+        case 15:
+            outputNumber.splice(outputNumber.indexOf(digit), 1, 'F');
             break;
     }
 }
