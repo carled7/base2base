8c024acab8f01d70d07268cc1f8be1cce686d84d
carled7 <carlos_ed03@outlook.com>
2021-05-14 13:45:03 -0300

    Fix share message

--- script.js
+++ script.js
@@ -378,7 +378,7 @@ function shareSite(socialMedia) {
 
     let socialMediaUrl;
     let urlSite = document.location.href;
-    let postTitle = encodeURI('Hey, check this out, a real time base converter:');
+    let postTitle = encodeURI('Hey, check this out:');
     let shareButton = document.getElementById(`${socialMedia}-btn`);
 
     switch (socialMedia) {
